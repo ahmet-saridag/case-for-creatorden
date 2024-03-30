eeb4f7a4d201f9f8210c28ddccbed0130f7c2c05
AHMET SARIDAG <ahmet.saridagj@gmail.com>
2024-03-30 14:50:01 +0300

    endpoint changed.

--- app/https.ts
+++ app/https.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const getInfluencerMockData = async () => {
+  //   const endpoint =
+  //     (process.env.NEXT_PUBLIC_SITE_URL ??
+  //       process?.env?.NEXT_PUBLIC_VERCEL_URL ??
+  //       "http://localhost:3000") + "/api/getMockData";
   const endpoint =
-    (process.env.NEXT_PUBLIC_SITE_URL ??
-      process?.env?.NEXT_PUBLIC_VERCEL_URL ??
-      "http://localhost:3000") + "/api/getMockData";
+    "https://case-for-creatorden.vercel.app" + "/api/getMockData";
   const { data } = await axios.post(endpoint);
   return data;
 };
